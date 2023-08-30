fb965facb24ca0382cccba78ab1d468993b10e6d
axelbriandip <axelbriandip.rg@gmail.com>
2023-08-30 03:18:03 -0300

    bkg animated done

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import './css/main.css';
 // components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import BackgroundPage from './components/BackgroundPage';
 
 function App() {
   return (
     <div className="App">
+      <BackgroundPage/>
       <Navbar/>
       <Home/>
     </div>
---
+++ src/components/BackgroundPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BackgroundPage = () => {
+    return (
+        <section className="wrapper">
+            {/* background page */}
+            <span className="item-bkg"><i className="fa-solid fa-laptop-code"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-keyboard"></i></span>
+            <span className="item-bkg"><i class="fa-brands fa-node-js"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-robot"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-microchip"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-power-off"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-database"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-network-wired"></i></span>
+            <span className="item-bkg"><i class="fa-brands fa-react"></i></span>
+            <span className="item-bkg"><i class="fa-solid fa-computer-mouse"></i></span>
+        </section>
+    );
+};
+export default BackgroundPage;
