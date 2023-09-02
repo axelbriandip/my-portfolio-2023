c8c2cbab1a11c13074ab8190dc5872b790c0a826
axelbriandip <axelbriandip.rg@gmail.com>
2023-09-02 01:46:51 -0300

    section contact done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import FormContact from "./Form-contact";
 
 const Contact = () => {
     return (
-        <section className="container-contact">
-            <div className="text-contact col-12">
-                <h2><i class="fa-solid fa-message"></i> | Contacto</h2>
-                {/* <p>Estos son algunos de mis trabajos..</p> */}
-            </div>
-            <div className="cards-contact">
-                {/* form */}
+        <section className="container-contact row" id='goto-contact'>
+            <h2 className="hover-line col-12"><i class="fa-solid fa-message"></i> Contacto</h2>
+            <div className="content-contact col-12">
+                <div className="info-contact">
+                    <p>
+                    Espero que haya sido de tu agrado todo lo que viste en el portafolio y te haya despertado el interés en mi trabajo, ya que todo está hecho con muchísimo esfuerzo y dedicación.
+                    <br /><br />
+                    Si llegaste hasta acá y estas interesado/a en saber cómo puedo ayudar a tu empresa o tenes un proyecto en el que creas que pueda encajar, te invito a dejarme un mensaje y con gusto me pondré en contacto contigo a la brevedad.
+                    <br /><br /><br />
+                    <i class="fa-solid fa-location-dot"></i> Tierra del Fuego, Argentina<br />
+                    <i class="fa-solid fa-phone"></i> +54 2964-569727<br />
+                    <i class="fa-solid fa-envelope"></i> axelbriandip.rg@gmail.com
+                    </p>
+                </div>
+                <div className="message-contact">
+                    <FormContact/>
+                </div>
             </div>
         </section>
     );
---
+++ src/components/Form-contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const FormContact = () => {
+    return (
+        <form action="https://formsubmit.co/axelbriandip.rg@gmail.com" method="POST">
+            <div className="group-input">
+                <label htmlFor="">Nombre</label>
+                <input type="text" placeholder="¿Cómo te llamas?" name="name" />
+            </div>
+            <div className="group-input">
+                <label htmlFor="">Correo</label>
+                <input type="text"  placeholder="¿Cuál es tu correo electrónico?" name="mail"/>
+            </div>
+            <div className="group-input">
+                <label htmlFor="">Mensaje</label>
+                <textarea placeholder="¿Qué tiene para dedcirme?" id="" cols="30" rows="10" name="message"></textarea>
+            </div>
+            <button class="btn btn-contact" type="submit">¡Enviar mensaje!</button>
+
+            {/* cambiar template */}
+            <input type="hidden" name="_template" value="box"/>
+            {/* valor por default del asunto */}
+            <input type="hidden" name="_subject" value="&#128235; ¡Nuevo mensaje del portafolio!"/>
+            {/* ¿A dónde va? */}
+            {/* <input type="hidden" name='_next' value='https://axelbriandip.netlify.app/'/> */}
+            <input type="hidden" name='_next' value='http://localhost:3000/'/>
+            {/* Sin captcha */}
+            <input type="hidden" name='_captcha' value={false} />
+        </form>
+    );
+};
+
+export default FormContact;
